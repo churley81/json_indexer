7031efa371a62f2b7618847ac38db708c3bd2e7b
Chris Hurley <christopher.hurley@outlook.com>
2022-02-08 15:08:34 -0500

    Restructured the index_object method to make the output more uniform.  Made necessary changes to the Indexed class.

--- indexer.py
+++ indexer.py
@@ -5,8 +5,7 @@ class Indexed:
         indexed = index_object(json_string)
         self.property_names = indexed['property_names']
         self.property_parents = indexed['property_parents']
-        self.container_names = indexed['container_names']
-        self.container_parents = indexed['container_parents']
+        self.property_types = indexed['property_types']
         self.container_references = indexed['container_references']
 
 class InvalidJSONForIndexingError(Exception):
@@ -19,47 +18,52 @@ def index_object(json_string):
         raise InvalidJSONForIndexingError
 
     container_types = (tuple, list, dict)
-    container_name_list = []
     container_reference_list = []
-    container_parent_list = []
     property_parent_list = []
     property_name_list = []
-    future_container_index = 1
+    property_type_list = []
+    container_to_property_map = {}
+
     container_index = 0
+    future_container_index = 1
+    property_index = 1
 
     container_reference_list.append(data)
-    container_parent_list.append(0)
-    container_name_list.append("")
+    property_parent_list.append(-1)
+    property_name_list.append('')
+    property_type_list.append(type(data))
+    container_to_property_map[0] = 0
 
     while container_index < future_container_index:
-        parent_container_name = container_name_list[container_index]
         container = container_reference_list[container_index]
         if not isinstance(container, list):
             base_keys = container.keys()
             for key in base_keys:
+                property_parent_list.append(container_to_property_map[container_index])
+                property_name_list.append(key)
+                property_type_list.append(type(container[key]))
                 if isinstance(container[key], container_types):
-                    container_name_list.append(parent_container_name + '/' + str(key))
-                    container_parent_list.append(container_index)
                     container_reference_list.append(container[key])
+                    container_to_property_map[future_container_index] = property_index
                     future_container_index += 1
-                else:
-                    property_parent_list.append(container_index)
-                    property_name_list.append(key)
+                property_index += 1
         else:
-            parent_container_name = parent_container_name + '/' + "Array"
             element_index = 0
             for element in container:
+                property_parent_list.append(container_to_property_map[container_index])
+                property_name_list.append(element_index)
+                property_type_list.append(type(element))
                 if isinstance(element, container_types):
-                    container_name_list.append(parent_container_name + '/' + str(element_index))
-                    container_parent_list.append(container_index)
                     container_reference_list.append(element)
+                    container_to_property_map[future_container_index] = property_index
                     future_container_index += 1
-                    element_index += 1
-                else:
-                    pass
+                property_index += 1
+                element_index += 1
         container_index += 1
+
+
+
     return {"property_names": property_name_list,
             "property_parents": property_parent_list,
-            "container_names": container_name_list,
-            "container_parents": container_parent_list,
+            "property_types": property_type_list,
             "container_references": container_reference_list}
